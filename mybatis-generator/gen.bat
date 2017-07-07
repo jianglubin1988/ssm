41b390b278e9d3a6cc953245b2677fb318ad4149
jianglubin <jiang.lubin@71dai.com>
2017-07-07 10:12:12 +0800

    create empty ssm project

---
+++ mybatis-generator/gen.bat
@@ -0,0 +1,3 @@
+@echo off
+java -jar mybatis-generator-core-1.3.5.jar -configfile generatorConfig.xml -overwrite
+pause
---
+++ src/main/java/ssm/com/test/IDao/TestMapper.java
@@ -0,0 +1,17 @@
+package ssm.com.test.IDao;
+
+import ssm.com.test.domain.Test;
+
+public interface TestMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Test record);
+
+    int insertSelective(Test record);
+
+    Test selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Test record);
+
+    int updateByPrimaryKey(Test record);
+}
