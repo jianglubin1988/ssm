4b67dc1e0ef1eecdd84cb4e69b0d2161cac8d238
jianglubin <jiang.lubin@71dai.com>
2017-07-10 09:25:33 +0800

    add ssm.bat

--- ssm.bat
+++ ssm.bat
@@ -6,7 +6,7 @@ if %input% == 1 (
 	mvn install
 )
 if %input% == 2 (
-	xcopy "D:\GitWorkspace\java\ssm\target\ssm.war" "D:\Tomcat 8.0\webapp\" /Y
+	xcopy "D:\GitWorkspace\java\ssm\target\ssm.war" "D:\Tomcat 8.0\webapps\" /Y
 )
 if %input% == 3 (
 	cd D:\Tomcat 8.0\bin\
