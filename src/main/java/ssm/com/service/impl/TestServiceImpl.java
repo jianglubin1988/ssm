dab41bee59fd9d99d507182ccf826cf5565c7936
jianglubin1988 <jianglubin1988@163.com>
2017-07-18 14:05:15 +0800

    updatge

--- src/main/java/ssm/com/service/impl/TestServiceImpl.java
+++ src/main/java/ssm/com/service/impl/TestServiceImpl.java
@@ -14,36 +14,30 @@ public class TestServiceImpl implements TestService{
 	@Resource
 	private TestMapper testMapper;
 	
-	@Override
 	public int deleteByPrimaryKey(Integer id) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
 	public int insert(Test record) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
 	public int insertSelective(Test record) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
 	public Test selectByPrimaryKey(Integer id) {
 		return this.testMapper.selectByPrimaryKey(id);
 	}
 
-	@Override
 	public int updateByPrimaryKeySelective(Test record) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
 	public int updateByPrimaryKey(Test record) {
 		// TODO Auto-generated method stub
 		return 0;
